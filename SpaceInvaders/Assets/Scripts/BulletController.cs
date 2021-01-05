0de9565fdf82f1f012aabc7ebcf2ced21186895f
emilypace26 <74545401+emilypace26@users.noreply.github.com>
2021-01-05 13:07:07 +0100

    created background and scripts
    added bullet and player scripts

---
+++ SpaceInvaders/Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+private Transform Bullet;
+public float Speed;
+
+    // Start is called before the first frame update
+    void Start() {
+        Bullet = GetComponent<Transform> ();
+    }
+
+    void FixedUpdate(){
+        Bullet.position += Vector3.up* Speed;
+
+        if (Bullet.position.y >= 10)
+            Destroy (gameObject);
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ SpaceInvaders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+private Transform Player;
+public float speed;
+public float maxBound, minBound;
+
+
+public GameObject shot;
+public Transform shotSpawn;
+public float fireRate;
+private float nextFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+    Player = GetComponent<Transform> ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    // maximum and minimum bound of player when moving
+    {
+        float h = Input.GetAxis("Horizontal");
+        if(Player.position.x < minBound && h < 0)
+        h = 0;
+        else if (Player.position.x > maxBound && h > 0)
+        h = 0;
+
+        Player.position += Vector3.right * h * speed;
+    }
+}
+
+
+
+
+
+
+
